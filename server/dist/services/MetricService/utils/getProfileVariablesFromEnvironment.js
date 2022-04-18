dab0148464cd74a689bcc069e47e7ab5d755a611
Michael Irvine <michael.j.irvine@gmail.com>
2022-04-18 19:31:19 -0400

    adds utils directory

---
+++ server/dist/services/MetricService/utils/getProfileVariablesFromEnvironment.js
@@ -0,0 +1,24 @@
+import { Warehouse } from '../DbtLocalMetricService.js';
+const getProfileVariablesFromEnv = () => {
+    const CREDENTIAL_KEYS = [
+        'USER',
+        'PASSWORD',
+        'TYPE',
+        'PROJECT_ID',
+        'PRIVATE_KEY_ID',
+        'PRIVATE_KEY',
+        'CLIENT_EMAIL',
+        'CLIENT_ID',
+        'AUTH_URI',
+        'TOKEN_URI',
+        'AUTH_PROVIDER_X509_CERT_URL',
+        'CLIENT_X509_CERT_URL',
+    ];
+    const profileVariables = Object.fromEntries(Object.entries(process.env).filter(([key]) => key.startsWith('DBT_PROFILE_') &&
+        !CREDENTIAL_KEYS.includes(key.replace('DBT_PROFILE_', ''))));
+    const credentials = Object.fromEntries(Object.entries(process.env).filter(([key]) => key.startsWith('DBT_PROFILE_') &&
+        CREDENTIAL_KEYS.includes(key.replace('DBT_PROFILE_', ''))));
+    return Object.assign(Object.assign({}, profileVariables), { type: profileVariables.type || Warehouse.POSTGRES, credentials: Object.assign({}, credentials) });
+};
+export default getProfileVariablesFromEnv;
+//# sourceMappingURL=getProfileVariablesFromEnvironment.js.map
---
+++ server/src/services/MetricService/utils/getProfileVariablesFromEnvironment.ts
@@ -0,0 +1,42 @@
+import {DbtProfile, Warehouse} from '../DbtLocalMetricService.js';
+
+const getProfileVariablesFromEnv = (): DbtProfile => {
+  const CREDENTIAL_KEYS = [
+    'USER',
+    'PASSWORD',
+    'TYPE',
+    'PROJECT_ID',
+    'PRIVATE_KEY_ID',
+    'PRIVATE_KEY',
+    'CLIENT_EMAIL',
+    'CLIENT_ID',
+    'AUTH_URI',
+    'TOKEN_URI',
+    'AUTH_PROVIDER_X509_CERT_URL',
+    'CLIENT_X509_CERT_URL',
+  ];
+
+  const profileVariables = Object.fromEntries(
+    Object.entries(process.env).filter(
+      ([key]) =>
+        key.startsWith('DBT_PROFILE_') &&
+        !CREDENTIAL_KEYS.includes(key.replace('DBT_PROFILE_', ''))
+    )
+  );
+
+  const credentials = Object.fromEntries(
+    Object.entries(process.env).filter(
+      ([key]) =>
+        key.startsWith('DBT_PROFILE_') &&
+        CREDENTIAL_KEYS.includes(key.replace('DBT_PROFILE_', ''))
+    )
+  ) as Record<string, string>;
+
+  return {
+    ...profileVariables,
+    type: (profileVariables.type as Warehouse) || Warehouse.POSTGRES,
+    credentials: {...credentials},
+  };
+};
+
+export default getProfileVariablesFromEnv;
